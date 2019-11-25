4ccf3b643253b270124fa838111c5b6117b2485e
Angel Abrego <563784+aabrego@users.noreply.github.com>
2019-11-25 07:33:33 -0800

    TEST. Reference harcoded property

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var jsonString;
 try {
   jsonString = core.getInput('json-string');
   const resultObject = JSON.parse(jsonString);
-  core.setOutput('object', resultObject);
+  core.setOutput('object', resultObject.result.url);
 
 } catch (error) {
   core.setFailed(error.message + '\n\n' + jsonString);
